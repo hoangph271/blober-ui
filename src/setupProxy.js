92f001dedd9642f438e11dfca28e82032ba355a6
Huy Hoàng Phan <hoangph271@gmail.com>
2021-02-14 14:30:55 +0700

    [🍑] Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     '/api/v1',
     createProxyMiddleware({
-      target: 'http://192.168.0.102:3000',
-      changeOrigin: true,
+      target: 'http://10.0.10.2:3000',
+      changeOrigin: true
     })
   )
 }
